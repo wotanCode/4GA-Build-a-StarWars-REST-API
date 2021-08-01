02d3a0a0b33b7253741d0d44d141f3c977d99a4c
Pedro Yanez <pedro_y91@outlook.com>
2021-08-01 13:44:31 +0000

    agregado favoritos planet

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, People, Planet, Vehicle, FavoriteVehicle, FavoritePeople
+from models import db, User, People, Planet, Vehicle, FavoriteVehicle, FavoritePeople, FavoritePlanet
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -17,6 +17,7 @@ def setup_admin(app):
     admin.add_view(ModelView(Vehicle, db.session))
     admin.add_view(ModelView(FavoriteVehicle, db.session))
     admin.add_view(ModelView(FavoritePeople, db.session))
+    admin.add_view(ModelView(FavoritePlanet, db.session))
     
 
     # You can duplicate that line to add mew models
--- src/models.py
+++ src/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     favoriteVehicle = db.relationship ('FavoriteVehicle', backref="user", lazy=True) #relacion uno a muchos
     favoritePeople = db.relationship ('FavoritePeople', backref="user", lazy=True)
+    favoritePlanet = db.relationship ('FavoritePlanet', backref="user", lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -18,12 +19,15 @@ class User(db.Model):
         return list(map(lambda vehicle: vehicle.serialize(),self.favoriteVehicle))
     def get_favPeople(self):
         return list(map(lambda people: people.serialize(),self.favoritePeople))
+    def get_favPlanet(self):
+        return list(map(lambda planet: planet.serialize(),self.favoritePlanet))
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             "favorites_vehicles": self.get_favVehicles(),
-            "favorites_people": self.get_favPeople()
+            "favorites_people": self.get_favPeople(),
+            "favorites_planet": self.get_favPlanet()
             # do not serialize the password, its a security breach
         }
 class People(db.Model):
@@ -37,7 +41,7 @@ class People(db.Model):
     id_planet = db.Column(db.Integer, db.ForeignKey('planet.id'))
     #RELACIONES
     planet = db.relationship ('Planet', lazy=True, backref="people", uselist=False) #relacion 1 a 1 el backref no es obligatorio aqui
-    favoriteUser = db.relationship ('FavoritePeople', backref="people", lazy=True) #relacion uno a muchos ULTIMA LINEA
+    favoriteUser = db.relationship ('FavoritePeople', backref="people", lazy=True) #relacion uno a muchos
     
     def __repr__(self):
         return '<People %r>' % self.name
@@ -58,6 +62,8 @@ class Planet(db.Model):
     climate = db.Column(db.String(80), unique=False, nullable=False)
     gravity = db.Column(db.String(80), unique=False, nullable=False)
     #id_people = db.Column(db.Integer, db.ForeignKey('people.id'))
+    #RELACION
+    favoriteUser = db.relationship ('FavoritePlanet', backref="planet", lazy=True) #relacion uno a muchos
 
     def __repr__(self):
         return '<Planet %r>' % self.name
@@ -133,4 +139,22 @@ class FavoritePeople(db.Model):
             #"user_email": self.user.email,
             "people_id": self.people_id,
             "Name": self.people.name
+        }
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favoritePlanet'
+    id = db.Column(db.Integer, primary_key=True)
+    #GUARDANDO RELACIONES
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            #"user_email": self.user.email,
+            "planet_id": self.planet_id,
+            #"Name": self.planet.name
         }
